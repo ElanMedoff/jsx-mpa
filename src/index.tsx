d1e88fb67c4e9ad439b54b224da7f7b3d8fc90c2
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-01-20 19:56:11 -0500

    remove unused route

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import express from "express";
 import { clientPackages } from "./client-packages/index";
 import recipeRecipeRoute from "./routes/recipe/:recipe";
 import recipesRoute from "./routes/recipes/index";
-import recipesSearchRoute from "./routes/recipes/search/index";
 
 const app = express();
 const port = 5858;
@@ -15,7 +14,6 @@ app.use("/dist-client", express.static("dist-client"));
 
 app.use("/", recipeRecipeRoute);
 app.use("/", recipesRoute);
-app.use("/", recipesSearchRoute);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
